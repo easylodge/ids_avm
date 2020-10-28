0da3dd1b077ad0a4561f8f81e27f22fe2d5bea38
MonsieurSlim <luccy559slim@gmail.com>
2020-10-28 12:43:18 +0100

    Minor change to config class

--- lib/ids_avm/configuration.rb
+++ lib/ids_avm/configuration.rb
@@ -1,8 +1,10 @@
-class IdsAvm::Configuration
-  attr_accessor :api_key, :url
+module IdsAvm
+  class Configuration
+    attr_accessor :api_key, :url
 
-  def initialize(args = {})
-    @api_key = args[:api_key] || ENV['IDS_AVM_API_KEY']
-    @url = args[:url] || ENV['IDS_AVM_URL']
+    def initialize(args = {})
+      @api_key = args[:api_key] || ENV['IDS_AVM_API_KEY']
+      @url = args[:url] || ENV['IDS_AVM_URL']
+    end
   end
 end
