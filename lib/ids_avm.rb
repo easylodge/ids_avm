a5bebfe4cd1536d53a42ed6e7d080038dec05563
MonsieurSlim <luccy559slim@gmail.com>
2020-10-28 12:36:27 +0100

    Revert base class changes

--- lib/ids_avm.rb
+++ lib/ids_avm.rb
@@ -5,14 +5,15 @@ require 'ids_avm/version'
 require 'httparty'
 require 'ids_avm/railtie' if defined?(Rails)
 
-module IdsAvm
-  extend self
+class IdsAvm
 
-  def configuration
-    @configuration ||= Configuration.new
-  end
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
 
-  def configure
-    yield(configuration)
+    def configure
+      yield(configuration)
+    end
   end
 end
